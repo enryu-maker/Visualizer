96090f89fedcabd96e48b2c94e9dc3608c5501ff
enryu-maker <akifkhan60067@gmail.com>
2021-05-06 22:20:08 +0530

    takes input and draw histogram

---
+++ dataAlgo.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+class Data():
+    def __init__(self):
+        self.fileName=input('Enter the file name-->')
+    def checking_columns(self):
+        self.df=pd.read_csv(self.fileName)
+        self.usable_df=self.df.dropna()
+        details=self.df.dtypes
+        self.column_detail=self.df.columns
+    def histo(self):
+        for x in self.column_detail:
+            plt.figure(figsize = (10, 7))
+            a = self.usable_df[x]
+            plt.hist(a, bins = 20, color = "green")
+            plt.title(x)
+            plt.xlabel(x)
+            plt.ylabel("Frequency")
+            plt.show()
+        
+D=Data()
+if __name__=='__main__':
+    D.checking_columns()
+    D.histo()
